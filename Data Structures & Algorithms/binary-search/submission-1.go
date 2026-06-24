feb405e9c2a9e2a63277309916ea79f0645c3192
vitormsantana <55901438+vitormsantana@users.noreply.github.com>
2026-06-24 00:23:07 -0300

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.go
@@ -0,0 +1,21 @@
+func search(nums []int, target int) int {
+    l := 0
+    r := len(nums) - 1
+    mid := 0
+
+    for l <= r {
+        mid = l + (r - l) / 2
+
+        if nums[mid] == target {
+            return mid
+        }
+
+        if nums[mid] < target {
+            l = mid + 1
+        } else {
+            r = mid - 1
+        }
+    }
+
+    return -1
+}

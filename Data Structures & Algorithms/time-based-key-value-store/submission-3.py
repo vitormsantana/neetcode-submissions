a089a1a5b20fab936d4b512b71e5bb26f92e3713
vitormsantana <55901438+vitormsantana@users.noreply.github.com>
2026-06-24 18:28:09 -0300

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,53 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if self.store.get(key):
+            self.store[key].append([value, timestamp])
+        else:
+            self.store[key] = [[value, timestamp]]
+
+    def get(self, key: str, timestamp: int) -> str:
+        
+        print(f'want key {key}')
+        
+        if key not in self.store:
+            return ""
+
+        data = self.store[key]
+        
+        if len(data) == 1:
+            if data[0][1] <= timestamp:
+                return data[0][0]
+            else:
+                return ""
+
+        maxx = -float('inf')
+        returnn = ''
+
+        print(f'timestamp: {timestamp}')
+        print(f'self.store[key]: {self.store[key]}')
+        
+        l = 0
+        r = len(data) - 1
+
+        print(f'l: {l}')
+        print(f'r: {r}')
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            print(f'mid: {mid}')
+            if data[mid][1] == timestamp:
+                print(f'data[mid][0]: {data[mid][0]}')
+                print('-------')
+                return data[mid][0]
+
+            if data[mid][1] < timestamp:
+                l = mid + 1
+                maxx = max(maxx, data[mid][1])
+                returnn = data[mid][0]
+            else:
+                r = mid - 1
+        return returnn
